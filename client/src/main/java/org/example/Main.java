8f46c5240c764c0d379ad8698d47fac5be46b8dd
AntsInMyEy3sJohnson <48876328+AntsInMyEy3sJohnson@users.noreply.github.com>
2024-06-13 15:53:04 +0200

    Introduce server and client projects

---
+++ client/src/main/java/org/example/Main.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.nio.serialization.genericrecord.GenericRecordBuilder;
+
+
+public class Main {
+
+    private static enum InteractionMode {
+        INSERT,
+        DELETE,
+        READ
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        spawnClientOnCluster("hazelcastplatform-0", "localhost:5701", InteractionMode.INSERT);
+
+        Thread.sleep(2000);
+
+        spawnClientOnCluster("hazelcastplatform-1", "localhost:5702", InteractionMode.READ);
+
+    }
+
+    private static void spawnClientOnCluster(String targetClusterName, String targetClusterAddress, InteractionMode m) throws InterruptedException {
+
+        var clientConfig = new ClientConfig();
+        clientConfig.setClusterName(targetClusterName);
+
+        var clientNetworkConfig = clientConfig.getNetworkConfig();
+        clientNetworkConfig.addAddress(targetClusterAddress);
+        clientNetworkConfig.setSmartRouting(false);
+
+        var client = HazelcastClient.newHazelcastClient(clientConfig);
+
+        Thread.sleep(1000);
+
+        var mapName = "htp_test-0";
+        var persistenceEnabledMap = client.getMap(mapName);
+
+        for (int i = 0; i < 10; i++) {
+            switch (m){
+                case INSERT -> {
+                    var genericRecord = GenericRecordBuilder.compact("htp")
+                            .setString("value", "awesome-value-" + i)
+                            .build();
+                    persistenceEnabledMap.put(i, genericRecord);
+                    System.out.printf("put value into map '%s' for key '%d': %s\n", mapName, i, genericRecord);
+                }
+                case DELETE -> persistenceEnabledMap.delete(i);
+                case READ -> {
+                    var valueFromMap = persistenceEnabledMap.get(i);
+                    System.out.printf("read value from map '%s': %s\n", mapName, valueFromMap);
+                }
+            }
+            Thread.sleep(100);
+        }
+
+    }
+
+}
---
+++ server/src/main/java/org/example/Main.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.DataConnectionConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MapStoreConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        var dataConnectionName = "hzpersistence";
+        var mapName = "htp_test-0";
+        var mapStoreConfig = assembleMapStoreConfig(dataConnectionName);
+
+        var numHazelcastInstances = 2;
+        HazelcastInstance[] hzInstances = new HazelcastInstance[numHazelcastInstances];
+        var basePort = 5701;
+        for (int i = 0; i < numHazelcastInstances; i++) {
+            // Local instances will form distinct clusters, so n(instances) = n(clusters)
+            hzInstances[i] = startNewHazelcastInstance("hazelcastplatform-" + i, dataConnectionName, mapName, mapStoreConfig, basePort + i);
+        }
+
+        while (true) {
+            for (var hzInstance: hzInstances) {
+                var m = hzInstance.getMap(mapName);
+                System.out.printf("'%s': map '%s' currently contains %d element(-s)\n", hzInstance.getConfig().getClusterName(), mapName, m.size());
+            }
+            System.out.println();
+            Thread.sleep(2000);
+        }
+
+    }
+
+    private static HazelcastInstance startNewHazelcastInstance(String clusterName, String dataConnectionName, String mapName, MapStoreConfig mapStoreConfig, int port) {
+
+        var hzConfig = assembleHazelcastConfig(clusterName, port);
+        hzConfig.addDataConnectionConfig(assembleDataConnectionConfig(dataConnectionName));
+
+        hzConfig.addMapConfig(assembleMapConfig(mapName, mapStoreConfig));
+
+        return Hazelcast.newHazelcastInstance(hzConfig);
+
+    }
+
+    private static MapConfig assembleMapConfig(String mapName, MapStoreConfig mapStoreConfig) {
+
+        return new MapConfig(mapName)
+                .setMapStoreConfig(mapStoreConfig);
+
+    }
+
+    private static MapStoreConfig assembleMapStoreConfig(String dataConnectionName) {
+
+        return new MapStoreConfig()
+                .setClassName("com.hazelcast.mapstore.GenericMapStore")
+                .setProperty("data-connection-ref", dataConnectionName);
+
+    }
+
+    private static DataConnectionConfig assembleDataConnectionConfig(String name) {
+
+        return new DataConnectionConfig(name)
+                .setType("JDBC")
+                .setProperty("jdbcUrl", "jdbc:mysql://localhost:30306/hzpersistence")
+                .setProperty("user", "hazman")
+                .setProperty("password", "also-super-secret")
+                .setShared(false);
+
+    }
+
+    private static Config assembleHazelcastConfig(String clusterName, int port) {
+
+        var hzConfig = new Config();
+        hzConfig.setClusterName(clusterName);
+
+        hzConfig.getMetricsConfig().setEnabled(false);
+        hzConfig.getJetConfig().setEnabled(true);
+        hzConfig.getNetworkConfig().setPort(port);
+
+        return hzConfig;
+
+    }
+}
